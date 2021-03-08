2c71dbd3a1c50ecb85861bf25a332d9216a050e0
zachmann00 <zach_mann@hotmail.com>
2021-03-08 10:07:25 -0500

    Finished assignment

---
+++ loopsExercise.js
@@ -0,0 +1,37 @@
+// 1
+for (i = 1; i <= 7; i++){
+    console.log(i);
+}
+
+// 2
+for (i = 5; i <= 25; i+=4){
+    console.log(i);
+}
+
+// 3a
+const wizards = [
+    "Harry Potter", 
+    "Hermione Granger", 
+    "Ron Weasley"];
+
+// 3b
+for (character of wizards){
+    console.log(character);
+}
+
+// 4a
+let harryPotterMovies = 0
+
+// 4b
+while (harryPotterMovies <=8){
+    console.log(harryPotterMovies);;
+    harryPotterMovies++;
+}
+
+//BONUS
+// 5a
+const hogwartsHouses = [
+    "Gryffindor", 
+    "Hufflepuff", 
+    "Ravenclaw", 
+    "Slytherin".]
